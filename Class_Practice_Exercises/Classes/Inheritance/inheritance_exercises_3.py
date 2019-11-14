5cbf0eecd3eb4e8e9df514cf8856976d9ef0e311
Mitch Hydrick <gary.hydrickm@gmail.com>
2019-11-14 15:39:03 -0600

    started inheritance exercises

---
+++ Class_Practice_Exercises/Classes/Inheritance/inheritance_exercises_3.py
@@ -0,0 +1,8 @@
+"""
+3. Person and Customer Classes
+    Write a class named Person with data attributes for a person’s name, address, and telephone number. Next, write a class named Customer that is a subclass of the Person class.
+    The Customer class should have a data attribute for a customer number and a Boolean data
+    attribute indicating whether the customer wishes to be on a mailing list. Demonstrate an
+    instance of the Customer class in a simple program.
+"""
+
---
+++ Class_Practice_Exercises/Classes/Inheritance/inheritance_exercises_4.py
@@ -0,0 +1,8 @@
+"""
+4. Chick-fil-a vs Popeye's Chicken Sandwich Quest
+    Using a superclass of Person with two subclasses Student and Instructor create a program that determines
+    who is going to go where and how we'll grade the sandwiches and determine a winner.
+    Get creative with this one. Add in some methods and have fun with it.I'll leave 
+    this open to see what you come up with.
+"""
+
